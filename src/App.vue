1e7aa3c6b71b965d98ca2a6a4594241d714ee3cb
MahmoudSiliman <modysiliman75@gmail.com>
2021-02-22 02:05:52 +0200

    add vue svg icons

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <font-awesome-icon icon="user-secret" />
+    <img alt="Vue logo" src="./assets/logo.png" /> <br>
+    <icon name="facebook-square" />
+    <icon name="twitter-square" />
+    <icon name="linkedin" />
+    <icon name="youtube-square" />
+    <icon name="instagram-square" />
+    <icon name="whatsapp-square" />
+    <icon name="user-circle" />
+    <icon name="dollar-sign" />
+    <icon name="at" />
+    <icon name="hashtag" />
+    <icon name="award" size="30px" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
-import "./plugins/fontawesome";
 import App from "./App.vue";
 import i18n from "./i18n";
+import icons from "v-svg-icons";
 
 Vue.config.productionTip = false;
+
+Vue.component("icon", icons);
 
 new Vue({
   i18n,
